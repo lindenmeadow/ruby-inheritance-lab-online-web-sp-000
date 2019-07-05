3fb7637f84044e97bc83c48ceab0c056afdad239
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-05 19:11:50 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,9 +2,12 @@ class User
 
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
+  def first_name=(first_name)
     @first_name = first_name
-    @last_name = last_name
+  end
+
+  def first_name
+    @first_name
   end
 
 end
